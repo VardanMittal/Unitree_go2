5461b10939af8f0adc8f5c85f6fb1386d4f3eb53
VardanMittal <vardanmittal000@gmail.com>
2024-11-07 13:50:37 +0530

    go2 description added

---
+++ go2_ws/build/go2_description/share/go2_description/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/vardan/Introduction-to-Robotics/Unitree_go2/go2_ws/build/go2_description"
